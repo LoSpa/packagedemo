fbda6f76d9275e2da25791ac92768eff0bf75bff
Laurent Spanu <l.spanu@issep.be>
2020-11-05 09:46:27 +0100

    Initial commit

---
+++ R/dev_history.R
@@ -0,0 +1,10 @@
+# Cacher ce fichier pour la création du package
+usethis::use_build_ignore("dev_history.R")
+
+# Définir une licence
+usethis::use_gpl3_license("lospa_ISSeP") #usethis::use_mit_license("ISSeP")
+
+# Créer une vignette (fichier .rmd et .md) pour documenter l'utilisation des fonctions package
+usethis::use_vignette("laurentspanu")
+
+usethis::use_git()
