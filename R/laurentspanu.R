a270db0c890d7dd4f47ea5815832e07d7b9cbe10
Laurent Spanu <l.spanu@issep.be>
2020-11-05 14:02:38 +0100

    add info package via laurentspanu.R

---
+++ R/laurentspanu.R
@@ -0,0 +1,12 @@
+#' laurentspanu : package de deux fonctions
+#'
+#' Le package apporte deux fonctions:
+#' imc et multi_import_excel
+#'
+#' @section Les fonctions:
+#' imc() et multi_import_excel
+#'
+#' @docType package
+#' @name laurentspanu
+
+NULL
