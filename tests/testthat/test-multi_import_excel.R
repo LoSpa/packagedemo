1a2c7f9193ad617a68d71ca785347c7ec0c38413
Laurent Spanu <l.spanu@issep.be>
2020-11-05 12:11:41 +0100

    corrections erreurs ... 0 error/2 warnings/0 notes

--- tests/testthat/test-multi_import_excel.R
+++ tests/testthat/test-multi_import_excel.R
@@ -1,5 +1,5 @@
 test_that("multi_import_excel works", {
 
-  #expect_equal_to_reference(multi_import_excel(system.file("extdata","datasets.xlsx",package = "laurentspanu")), "list")
+  expect_true(is.list(multi_import_excel(system.file("extdata","datasets.xlsx",package = "laurentspanu"))))
 
   })
